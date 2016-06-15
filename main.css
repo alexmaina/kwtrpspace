80f41076c78d7ec7180f43eb99444e4b05f90246
Alex Maina <alexmaina@afroscholar.info>
2016-06-15 15:25:52 +0300

    Update main.css

--- main.css
+++ main.css
@@ -4,7 +4,7 @@
 (KWTRPSPACE Version 1.0), is Manuscript management information system
 designed to manage manuscripts from conception to publication in peer-reviewed journals.
 This program is free software; you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License,
+of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License,
 or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
