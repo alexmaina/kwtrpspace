ba888fad1a85e022691635fc914695d140c8112d
Alex Maina <alexmaina@afroscholar.info>
2016-06-15 15:26:46 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,14 +6,13 @@
 (KWTRPSPACE Version 1.0), is Manuscript management information system
 designed to manage manuscripts from conception to publication in peer-reviewed journals.
 This program is free software; you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License,
+of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License,
 or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program;
-if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA or 
-check for license.txt at the root folder
+If not, see <http://www.gnu.org/licenses/>.
 /*****************************************************************************
 For any details please feel free to contact me at afroscholar@users.sourceforge.net
 Or for snail mail. P. O. Box 71044,Nairobi-00610, East Africa-Kenya.
